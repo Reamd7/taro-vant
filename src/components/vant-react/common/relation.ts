adbfea99b8b6fd6562a9f117f6c6254109e0fae9
reamd7 <reamd7@gmail.com>
2020-11-24 03:24:40 +0800

    update relation 基本能够支持类似React.children的能力了。

--- src/components/vant-react/common/relation.ts
+++ src/components/vant-react/common/relation.ts
@@ -2,7 +2,7 @@
  * 依赖rxjs的 BehaviorSubject
  */
 // import { BehaviorSubject } from 'rxjs'
-import { useEffect, useState, useMemo, useCallback } from '@tarojs/taro';
+import { useEffect, useState, useMemo, useCallback, useRef } from '@tarojs/taro';
 import { getContext } from './utils';
 import useUpdateEffect from 'src/common/hooks/useUpdateEffect';
 
@@ -40,14 +40,70 @@ const relationPropsMap = new Map<
 >()
 /**
  * 以 pid 为标识符，注入组件的 props 到外部的状态管理中。
+ *
+ * NOTE：
+ * ```
+ * nerv/src/render-queue.ts
+ * // 就是这个原因 pop ，所以更新的时候是逆序更新的，但是mount的时候是顺序挂载的。
+ * // NOTE : 这个如果要迁移到react中需要进行测试。
+ * function rerender(isForce) {
+      if (isForce === void 0) isForce = false;
+
+      var p;
+      var list = items;
+      items = [];
+      // tslint:disable-next-line:no-conditional-assignment
+      while (p = list.pop()) { // 因为这里的原因，所以这里更新是逆序的。
+          if (p._dirty) {
+              updateComponent(p, isForce);
+          }
+      }
+  }
+  ```
+ *
  * @param pid
  * @param props
  */
-export function useRelationPropsInject<T>(pid: string, relation: (props: T) => T, deps: any[]) {
+export function useRelationPropsInject<T extends {
+  index: number;
+  total: number;
+}>(pid: string, relation: (props: T) => T, deps: any[]) {
   const context = getContext();
   const _id = context ? `${context}_${pid}` : null;
+
+  const PropsListRef = useRef<Array<T>>([]);
+  const [PropsList, setPropsList] = useState<Array<T>>(PropsListRef.current);
+
+  const status = useRef<"update" | "mount">("mount");
+
+  const Fn = useCallback((props: T) => {
+    const newProps = relation(props);
+    const index = props.index;
+    // NOTE : 这个如果要迁移到react中需要进行测试。
+    if (status.current === "mount") {
+      if (index === 0) {
+        PropsListRef.current = Array(props.total);
+      }
+      PropsListRef.current[index] = (newProps);
+      if (index === (props.total - 1)) {
+        setPropsList(PropsListRef.current);
+        status.current = "update"
+      }
+    } else {
+      if (index === (props.total - 1)) {
+        PropsListRef.current = Array(props.total);
+      }
+      PropsListRef.current[index] = (newProps);
+      if (index === 0) {
+        setPropsList(PropsListRef.current);
+      }
+    }
+
+    return newProps
+  }, [relation, ...deps])
+
   const [state] = useState(() => {
-    const state = new BehaviorSubject(relation)
+    const state = new BehaviorSubject(Fn)
     if (_id) {
       relationPropsMap.has(_id) || relationPropsMap.set(_id, state);
     }
@@ -62,9 +118,11 @@ export function useRelationPropsInject<T>(pid: string, relation: (props: T) => T
     }
   }, [_id]); // 有了 id 之后 注入 map 中
 
-  useUpdateEffect(() => {
-    state.next(relation);
-  }, deps) // 通知更新
+  useEffect(() => {
+    state.next(Fn);
+  }, [...deps]) // 通知更新
+
+  return PropsList
 }
 /**
  * 获取 pid 标识符映射的 props
